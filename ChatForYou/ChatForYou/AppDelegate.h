14004045d8a55fa7279ab24ba8f01f64c34184e2
ie03 <songjinwei@yshjie.com>
2015-11-03 17:44:39 +0800

    初始化项目

---
+++ ChatForYou/ChatForYou/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChatForYou
+//
+//  Created by HMC on 15/11/3.
+//  Copyright © 2015年 SKing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChatForYou/ChatForYou/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ChatForYou
+//
+//  Created by HMC on 15/11/3.
+//  Copyright © 2015年 SKing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
